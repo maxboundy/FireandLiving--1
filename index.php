16538ba015d3ebd0de55ae5357eab71cc01aafd1
maxboundy <78894664+maxboundy@users.noreply.github.com>
2021-09-19 08:13:53 +0000

    V1

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php include("head_nav.html"); ?>
+        
+        <div class="box main">
+            <h2>Fire and Living®</h2>
+             </div>    <!-- / main -->
+        
+        <div class="box side">
+            <h2>Who we are...</h2>
+</div>  <!-- / side bar -->
+
+<?php include("footer.html"); ?>
